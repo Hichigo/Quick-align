e4eae16f16d8fc6110272c8521707907e5bd422c
Ivan <hichigo91@gmail.com>
2017-03-29 11:46:32 +0300

    set oridgin apply to selected

--- quick_align.py
+++ quick_align.py
@@ -42,7 +42,7 @@ class ObjectSetOrogin(bpy.types.Operator):
 
 	def execute(self, context):
 		if bpy.context.mode == 'EDIT_MESH':
-			bpy.ops.view3d.snap_cursor_to_active()
+			bpy.ops.view3d.snap_cursor_to_selected()
 			bpy.ops.object.editmode_toggle()
 			bpy.ops.object.origin_set(type = 'ORIGIN_CURSOR')
 			#bpy.ops.object.editmode_toggle()
