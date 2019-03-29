21264951e8b4e45d296e41f5c1e703df765dc157
Ivan <hichigo91@gmail.com>
2019-03-29 11:41:18 +0300

    fixed align function paremeter

--- functions.py
+++ functions.py
@@ -9,14 +9,14 @@ def align_XYZ(x, y, z, axisX, axisY, axisZ):
 
 	if bpy.context.mode == 'OBJECT':
 		# bpy.context.space_data.use_pivot_point_align = True
-		bpy.ops.transform.resize(value=(x, y, z), constraint_axis=(axisX, axisY, axisZ), constraint_orientation='GLOBAL', mirror=False, proportional='DISABLED', proportional_edit_falloff='SMOOTH', proportional_size=1)
+		bpy.ops.transform.resize(value=(x, y, z), constraint_axis=(axisX, axisY, axisZ), orient_matrix_type='GLOBAL', mirror=False, proportional='DISABLED', proportional_edit_falloff='SMOOTH', proportional_size=1)
 		# bpy.context.space_data.use_pivot_point_align = False
 	else:
-		bpy.ops.transform.resize(value=(x, y, z), constraint_axis=(axisX, axisY, axisZ), constraint_orientation='GLOBAL', mirror=False, proportional='DISABLED', proportional_edit_falloff='SMOOTH', proportional_size=1)
+		bpy.ops.transform.resize(value=(x, y, z), constraint_axis=(axisX, axisY, axisZ), orient_matrix_type='GLOBAL', mirror=False, proportional='DISABLED', proportional_edit_falloff='SMOOTH', proportional_size=1)
 
 	# bpy.context.space_data.pivot_point = piv
 	bpy.context.scene.tool_settings.use_transform_pivot_point_align = False
 
 
 def align_graph(x, y, z, axisX, axisY, axisZ):
-	bpy.ops.transform.resize(value=(x, y, z), constraint_axis=(axisX, axisY, axisZ), constraint_orientation='GLOBAL', mirror=False, proportional='DISABLED', proportional_edit_falloff='SMOOTH', proportional_size=1)
+	bpy.ops.transform.resize(value=(x, y, z), constraint_axis=(axisX, axisY, axisZ), orient_matrix_type='GLOBAL', mirror=False, proportional='DISABLED', proportional_edit_falloff='SMOOTH', proportional_size=1)
