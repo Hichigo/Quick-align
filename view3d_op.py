9726eea15d24e0b1be854a94c526d613cc26355b
Ivan <hichigo91@gmail.com>
2019-06-22 11:48:35 +0300

    moved drop to ground code to new file

--- view3d_op.py
+++ view3d_op.py
@@ -1,7 +1,5 @@
 import bpy
 
-from mathutils import Vector
-
 from . functions import align_XYZ
 
 
@@ -61,34 +59,4 @@ class VIEW3D_OT_align_z_slots(bpy.types.Operator):
 
 	def execute(self, context):
 		align_XYZ(1,1,0,False,False,True)
-		return {'FINISHED'}
-
-
-class VIEW3D_OT_drop_to_ground(bpy.types.Operator):
-	"""Drop to ground selected objects"""
-	bl_idname = "view3d.drop_to_ground"
-	bl_label = "Drop to ground"
-	bl_options = {'REGISTER', 'UNDO'}
-
-	@classmethod
-	def poll(cls, context):
-		return context.active_object is not None
-
-	def execute(self, context):
-		objects = bpy.context.selected_objects
-
-		for ob in objects:
-			ob.hide_set(True)
-			bHit, pos_hit, normal_hit, face_index_hit, obj_hit, matrix_world = context.scene.ray_cast(
-				view_layer=context.view_layer,
-				origin=ob.location,
-				direction=Vector((0,0,-1))
-			)
-
-			ob.hide_set(False)
-			ob.select_set(True)
-
-			if bHit:
-				ob.location = pos_hit
-
 		return {'FINISHED'}
